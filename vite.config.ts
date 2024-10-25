3902c83cf3bd4b9169ca5c0d7a78bf6d06064fcc
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2024-10-25 12:33:35 -0400

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from "vite-plugin-singlefile"
+/* import { viteSingleFile } from "vite-plugin-singlefile" */
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  viteSingleFile()
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: './dist/',
+        entryFileNames: 'script.js',
+        assetFileNames: 'style.css',
+      }
+    }
+  }
 })
